943d94838976a92031c895d6746a247eb353a7ca
Kateryan Ksondzyk <ketrinksion@gmail.com>
2022-11-20 22:56:12 +0200

    change2

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -2,6 +2,7 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
     }
 }
